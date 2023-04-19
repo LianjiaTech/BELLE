ea3bf027aa92fe9130ea495089ab17a74e2285f5
curvasong <curvasong@tecent.com>
2023-04-19 19:38:22 +0800

    update run_quant

--- gptq/run_quant.sh
+++ gptq/run_quant.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python bloom.py --model /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/Belle/BELLE-7B-2M --dataset wikitext2 --wbits 8 --groupsize 128 --save BELLE-7B-gptq/bloom7b-2m-8bit-128g.pt
+CUDA_VISIBLE_DEVICES=0 python bloom.py /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/Belle/BELLE-7B-2M wikitext2 --wbits 8 --groupsize 128 --save BELLE-7B-gptq/bloom7b-2m-8bit-128g.pt
